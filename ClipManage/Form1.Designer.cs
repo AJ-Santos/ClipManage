9f89fc43e23e6a976608cae3dc0bd5b1038de7c4
AntonioS <aj.santos128@gmail.com>
2015-01-08 22:25:33 -0400

    Added Source Code.

--- ClipManage/Form1.Designer.cs
+++ ClipManage/Form1.Designer.cs
@@ -1,39 +1,144 @@
 ﻿namespace ClipManage
 {
-	partial class Form1
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.clearButton = new System.Windows.Forms.Button();
+			this.pasteButton = new System.Windows.Forms.Button();
+			this.copyButton = new System.Windows.Forms.Button();
+			this.textClipboard = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.textClipboard, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(330, 341);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.clearButton);
+			this.flowLayoutPanel1.Controls.Add(this.pasteButton);
+			this.flowLayoutPanel1.Controls.Add(this.copyButton);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 309);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(324, 29);
+			this.flowLayoutPanel1.TabIndex = 0;
+			// 
+			// clearButton
+			// 
+			this.clearButton.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.clearButton.Location = new System.Drawing.Point(246, 3);
+			this.clearButton.Name = "clearButton";
+			this.clearButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.clearButton.Size = new System.Drawing.Size(75, 23);
+			this.clearButton.TabIndex = 3;
+			this.clearButton.Text = "Clear";
+			this.clearButton.UseVisualStyleBackColor = true;
+			this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+			// 
+			// pasteButton
+			// 
+			this.pasteButton.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pasteButton.Location = new System.Drawing.Point(165, 3);
+			this.pasteButton.Name = "pasteButton";
+			this.pasteButton.Size = new System.Drawing.Size(75, 23);
+			this.pasteButton.TabIndex = 2;
+			this.pasteButton.Text = "Paste";
+			this.pasteButton.UseVisualStyleBackColor = true;
+			this.pasteButton.Click += new System.EventHandler(this.pasteButton_Click);
+			// 
+			// copyButton
+			// 
+			this.copyButton.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.copyButton.Location = new System.Drawing.Point(84, 3);
+			this.copyButton.Name = "copyButton";
+			this.copyButton.Size = new System.Drawing.Size(75, 23);
+			this.copyButton.TabIndex = 1;
+			this.copyButton.Text = "Copy";
+			this.copyButton.UseVisualStyleBackColor = true;
+			this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+			// 
+			// textClipboard
+			// 
+			this.textClipboard.AcceptsReturn = true;
+			this.textClipboard.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.textClipboard.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textClipboard.Location = new System.Drawing.Point(3, 3);
+			this.textClipboard.Multiline = true;
+			this.textClipboard.Name = "textClipboard";
+			this.textClipboard.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textClipboard.Size = new System.Drawing.Size(324, 300);
+			this.textClipboard.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
-		}
+			this.ClientSize = new System.Drawing.Size(330, 341);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ClipManage";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
-		#endregion
-	}
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.Button pasteButton;
+		private System.Windows.Forms.Button clearButton;
+		private System.Windows.Forms.TextBox textClipboard;
+    }
 }
 
--- ClipManage/Form1.cs
+++ ClipManage/Form1.cs
@@ -1,20 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// ClipMange is a small Cilpboard manager. It can copy, paste, and clear the clipboard.
+// Logged build dates: 
+//		Start 12/28/13 @ 3:58pm End 12/28/13 @ 5:30pm
+//		Start 1/22/14 @ 9:00pm End 1/22/14 @
+// © 2014 Antonio Santos
 namespace ClipManage
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
+    public partial class Form1 : Form
+    {	
+
+		public static string clipText = Clipboard.GetText();
+
+        /// <summary>
+        /// Paste the contents of the clipboard once the program
+        /// starts.
+        /// </summary>
+        private void onProgramStart()
+        {
+            textClipboard.Text = clipText;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            onProgramStart();
+        }
+
+        // Clears the text box and the clipboard
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            textClipboard.Clear();
+        }
+
+        // Copies the text in the text box to the clipboard.
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+
+			if (textClipboard.Text == "")
+				{
+					const string message = "There is no text to copy to the Clipboard.";
+					const string caption = "Error!";
+					MessageBox.Show(message, caption,
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Error);
+
+				}
+			
+			else
+				{
+					Clipboard.SetText(textClipboard.Text);
+						// Display a MessageBox to inform the user that 
+						// the text has been copied to the Clipboard.
+						const string message = "Your text has been copied to the Clipboard.";
+						const string caption = "Text Copied"; 
+						MessageBox.Show(message, caption,
+										MessageBoxButtons.OK,
+										MessageBoxIcon.Information);
+				}
+        }
+
+        // Clears the text box and pastes the contents of clipboard.
+        private void pasteButton_Click(object sender, EventArgs e)
+        {
+			clipText = Clipboard.GetText();
+
+			if (clipText == textClipboard.Text)
+			{
+			const string message = "You have not entered any new text to paste.";
+			const string caption = "Error!";
+			MessageBox.Show(message, caption,
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Error);
+			}
+
+			else if (clipText.Length > 0)
+				{
+				textClipboard.Clear();
+				textClipboard.Text = clipText;
+				}
+        }
+
+		// When the user clicks the Close button.
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+			{
+			const string message =
+				"Are you sure that you would like to close the program?";
+			const string caption = "Program Closing";
+			var result = MessageBox.Show(message, caption,
+										 MessageBoxButtons.YesNo,
+										 MessageBoxIcon.Question);
+
+			// If the no button was pressed ... 
+			if (result == DialogResult.No)
+				{
+				// cancel the closure of the form.
+				e.Cancel = true;
+				}
+			}
 		}
-	}
-}
+    }
